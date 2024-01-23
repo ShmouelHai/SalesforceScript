c5bf3e7b1f5e5d5a658c6b3824eead71bb396669
Shmouel Hai Illouz <57444008+ShmouelHai@users.noreply.github.com>
2024-01-23 18:56:31 +0200

    Update ComparePackagesXML.py

--- Done and Improve/ComparePackagesXML.py
+++ Done and Improve/ComparePackagesXML.py
@@ -1,12 +1,14 @@
+# By Shmouel Illouz : Being lazy is knowing how to write scripts.
+
 import xml.etree.ElementTree as ET
 import pandas as pd
 import os
 from datetime import datetime
 
 if __name__ == "__main__":
-    # Demande à l'utilisateur de fournir le chemin du fichier XML
     print("If the output excel is open please close it before launch the script")
-    xmlROW_file_path = input("Entrez le chemin du fichier XML ROW: ")
+    print("The package to be compared has more than the comparing package, and we want to check what is missing.")
+    xmlROW_file_path = input("Please enter the path of the package to be compared: ")
 
     # Parsing XML
     tree = ET.parse(xmlROW_file_path)
@@ -22,7 +24,7 @@ if __name__ == "__main__":
 
     df = pd.DataFrame()
 
-    # Chaque type est défini par un name
+    # Each type is defined by a name
     for types in root:
         nameTypeList = ''
         membersList = []
@@ -47,12 +49,12 @@ if __name__ == "__main__":
         if nameTypeList == 'Dashboard':
             statusMembersList[-len(membersList):] = ["Scope To Define"] * len(membersList)
 
-    # Construction de finalGrandList après que statusMembersList a été construit 
+    # Construction of finalGrandList after that statusMembersList has been constructed
         for member in membersList:
             finalGrandList.append(member)
             finalGrandListMembers.append(nameTypeList)
 
-    xmlSFOA_file_path = input("Entrez le chemin du fichier XML SFOA: ")
+    xmlSFOA_file_path = input("Please enter the path of the comparing package: ")
     # Parsing XML
     tree = ET.parse(xmlSFOA_file_path)
     root = tree.getroot()
@@ -68,47 +70,42 @@ if __name__ == "__main__":
 
     for temp in finalGrandList:
         if temp in membersListSFOA:
-            SFOA_isInROW.append("In SFOA")
+            SFOA_isInROW.append("In Package")
         else:
-            SFOA_isInROW.append("Not in SFOA")
+            SFOA_isInROW.append("Not in Package")
             
-
-    # Création d'un DataFrame Pandas
     print("finalGrandList: " + str(len(finalGrandList)))
     print("finalGrandListMembers: " + str(len(finalGrandListMembers)))
     print("statusMembersList: " + str(len(statusMembersList)))
     print("SFOA_isInROW: " + str(len(SFOA_isInROW)))
     
     #Create the frame excel
-    df = pd.DataFrame({"Members": finalGrandList, "Type": finalGrandListMembers, "SFOA Status": statusMembersList, "SFOA package": SFOA_isInROW}) #, "Deployment Status": df['Deployment Status']
+    df = pd.DataFrame({"Members": finalGrandList, "Type": finalGrandListMembers, "SFOA Status": statusMembersList, "SFOA package": SFOA_isInROW}) 
 
-    # Comparaison entre SFOA Status et SFOA Package pour créer la nouvelle colonne
-    df['Deployment Status'] = 'OK'  # Initialise la colonne à "OK"
+    # Comparaison between SFOA Status and SFOA Package to create the new column
+    df['Deployment Status'] = 'OK'  # Initialise the column to "OK"
 
-    # Conditions pour mettre à jour la colonne en fonction des comparaisons
+    # Conditions to update the column according to comparaisons
     df.loc[(df['SFOA Status'] == 'Expected OK') & (df['SFOA package'] == 'Not in SFOA'), 'Deployment Status'] = 'NOT OK'
     df.loc[(df['SFOA Status'] == 'Out of Scope') & (df['SFOA package'] == 'In SFOA'), 'Deployment Status'] = 'NOT OK'
     df.loc[(df['SFOA Status'] == 'Not supported in SFOA') & (df['SFOA package'] == 'In SFOA'), 'Deployment Status'] = 'NOT OK'
     df.loc[(df['SFOA Status'] == 'Data Scope') & (df['SFOA package'] == 'In SFOA'), 'Deployment Status'] = 'NOT OK'
 
 
-
-
-    # Chemin du fichier Excel sur le bureau
+    # Output's Path to Desktop
     desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
     output_excel_path = os.path.join(desktop_path, "CompareXML.xlsx")
 
-    # Vérifier si le fichier Excel existe
+    # Check before if the excel already existing
     if os.path.exists(output_excel_path):
-        # Charger le fichier Excel existant avec les feuilles existantes
         with pd.ExcelWriter(output_excel_path, engine='openpyxl', mode='a') as writer:
-            # Ajouter le DataFrame comme une nouvelle feuille
+            # Add into a new sheet
             unique_suffix = 'Sheet' + str(datetime.now().strftime("%d%H%M%S"))
             df.to_excel(writer, sheet_name=unique_suffix , index=False)
     else:
-        # Si le fichier Excel n'existe pas, simplement écrire le DataFrame
+        # Create a new excel
         df.to_excel(output_excel_path, index=False)
 
     print(f"Excel file '{output_excel_path}' updated successfully.")
-    # Ouvrir le fichier Excel
+    # Open the exceel
     os.system(output_excel_path)
